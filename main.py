822444f138429dbca2f40c8eabf2b387c3de27e6
KNKHACK <knkhack365days@gmail.com>
2025-07-18 19:28:50 +0530

    log add

---
+++ main.py
@@ -0,0 +1,8 @@
+# write a calcuator  to add numbers
+def add_numbers(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def add_multiple_numbers(*args):
+    """Returns the sum of multiple numbers."""
+    return sum(args)                        
